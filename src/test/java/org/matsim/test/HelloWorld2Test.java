433a436d18d3d580c0e77382f382ecc8de7b2682
kturner <martins-turner@vsp.tu-berlin.de>
2018-11-13 10:25:27 +0100

    fix in line 46: new Controler(scenario) instead of (config). Now population test passes.

--- src/test/java/org/matsim/test/HelloWorld2Test.java
+++ src/test/java/org/matsim/test/HelloWorld2Test.java
@@ -43,8 +43,9 @@ public class HelloWorld2Test {
 			config.controler().setOverwriteFileSetting(OverwriteFileSetting.overwriteExistingFiles);
 			config.controler().setWriteEventsInterval(1);
 			Scenario scenario = ScenarioUtils.loadScenario(config);
-			Controler controler = new Controler(config);
+			Controler controler = new Controler(scenario);
 			controler.run();
+			
 			Config config2 = ConfigUtils.createConfig();
 			config2.plans().setInputFile(utils.getInputDirectory()+"/output_plans.xml.gz");
 			Scenario scenario2 = ScenarioUtils.loadScenario(config2);
